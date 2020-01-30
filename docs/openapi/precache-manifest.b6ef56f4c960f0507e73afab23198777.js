e69e667cb82c9a79b0b1291a1070ce995346181d
alvin <seogi777@gmail.com>
2020-01-30 11:52:01 +0900

    JENNIFER API 문서 동기화 (5.5.1)

---
+++ docs/openapi/precache-manifest.b6ef56f4c960f0507e73afab23198777.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e57b11722eb75f8226e48a9d04507b98",
+    "url": "/jennifer-developer-guide/openapi/index.html"
+  },
+  {
+    "revision": "bc6da56b72216a65b343",
+    "url": "/jennifer-developer-guide/openapi/static/css/2.4ed8dd72.chunk.css"
+  },
+  {
+    "revision": "5e9fb625626359cc3c7f",
+    "url": "/jennifer-developer-guide/openapi/static/css/main.e1dba005.chunk.css"
+  },
+  {
+    "revision": "bc6da56b72216a65b343",
+    "url": "/jennifer-developer-guide/openapi/static/js/2.a631ccfc.chunk.js"
+  },
+  {
+    "revision": "5e9fb625626359cc3c7f",
+    "url": "/jennifer-developer-guide/openapi/static/js/main.f68dd920.chunk.js"
+  },
+  {
+    "revision": "260f12f24894dd1e960e",
+    "url": "/jennifer-developer-guide/openapi/static/js/runtime~main.bbe1350e.js"
+  }
+]);
